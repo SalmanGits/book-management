e38a2541397241fb07944ba1270a84d2f0d1535c
SalmanGits <belalahmad10310@gmail.com>
2024-01-06 05:16:41 +0530

    signup and login api with auth and global error handler and some helper functions

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 const helmet = require("helmet")
+const morgan = require('morgan')
 const app = express()
 const { connectToDb } = require("./connection/db.js");
-const PORT = process.env.PORT || 4001
+const errorHandler = require("./middleware/errorHandler.js");
+const userRoutes = require("./routes/user.route.js")
+const PORT = process.env.PORT || 3000
+//middlewares
 app.use(cors())
 app.use(helmet())
+app.use(morgan("tiny"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+//db connection
 connectToDb()
+//routes
+app.use("/api/auth",userRoutes)
+//error handler
+app.use(errorHandler)
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
